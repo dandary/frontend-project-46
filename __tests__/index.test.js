3285b4cf9aab228c284092234787a115991c898a
Dandary <mukuro489@gmail.com>
2023-06-19 16:41:22 -0700

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,6 @@ import genDiff from '../src/index.js';
 const filename = fileURLToPath(import.meta.url);
 const dirName = dirname(filename);
 const readFile = (nameOfFile) => readFileSync(resolve(dirName, '..', '__fixtures__', nameOfFile), 'utf-8');
-
 const cases = [
   ['__fixtures__/file1.json', '__fixtures__/file2.json', readFile('stylishTest.txt')],
   ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', readFile('stylishTest.txt')],
@@ -18,7 +17,6 @@ const cases = [
   ['__fixtures__/file1.json', '__fixtures__/file2.json', readFile('jsonTest.txt'), 'json'],
   ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', readFile('jsonTest.txt'), 'json'],
 ];
-
 describe('output format', () => {
   test.each(cases)('difference %s and %s', (a, b, result, format = 'stylish') => {
     expect(genDiff(a, b, format)).toEqual(result);
